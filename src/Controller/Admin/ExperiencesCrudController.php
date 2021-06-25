2571693b9484ca2aa74b4adf027170396e64d7b2
Louison Dupont <dupontlouison@gmail.com>
2021-06-25 18:04:19 +0200

    Commit test

--- src/Controller/Admin/ExperiencesCrudController.php
+++ src/Controller/Admin/ExperiencesCrudController.php
@@ -6,6 +6,7 @@ use App\Entity\Experiences;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -19,6 +20,7 @@ class ExperiencesCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            AssociationField::new('user', 'Utilisateur'),
             TextField::new('name', 'Intitulé'),
             TextField::new('entreprise', 'Entreprise'),
             TextareaField::new('description', 'Description'),
